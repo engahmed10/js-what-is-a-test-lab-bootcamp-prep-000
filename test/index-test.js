f80ab4081e41b0f80308193f0f62d8292ba03a2f
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-12 05:05:55 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 var index = require("../index.js")
 
-describe('what-is-a-test', () => {
+describe('ahmed afandy', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
